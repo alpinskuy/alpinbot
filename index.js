a06b78b1dce8dab36336c2a69d6a8c51948defb4
alpinskuy <74120796+alpinskuy@users.noreply.github.com>
2020-11-08 07:42:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -251,7 +251,7 @@ else if (text == 'P'){
 conn.sendMessage(id, 'Ya?, ada yang bisa saya bantu? kalo bingung ketik #help ya say. Jngn Lupa Follow IG aing @pinnskuyy.' ,MessageType.text);
 }
 else if (text == 'oi){
-conn.sendMessage(id, 'oiit ketik help ya anjing..' ,MessageType.text);
+conn.sendMessage(id, 'oiit ketik #help ya anjing..' ,MessageType.text);
 }
 else if (text == '#stiker'){
 conn.sendMessage(id, 'Maaf saat ini tidak bisa mengakses fitur stiker..' ,MessageType.text);
